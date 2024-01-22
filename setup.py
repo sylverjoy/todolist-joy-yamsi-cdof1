fcc88c9ed277c9a7da1c10cf9d8870e493143031
sylverjoy <joyyamsi6@gmail.com>
2024-01-22 14:17:42 +0100

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{todo-list}',
+ version='1.0',
+ author='{Joy Sylver Yamsi T.}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
